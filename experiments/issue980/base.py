5b7263f9aa74d7ed057c5db532176f69c2bdfd6d
Michael Katz <ctpelok@gmail.com>
2021-08-22 16:26:55 +0000

    added exp files

--- experiments/issue980/base.py
+++ experiments/issue980/base.py
@@ -7,7 +7,7 @@ from lab.environments import LocalEnvironment, BaselSlurmEnvironment
 
 import common_setup
 from common_setup import IssueConfig, IssueExperiment
-#from relativescatter import RelativeScatterPlotReport
+from relativescatter import RelativeScatterPlotReport
 from itertools import combinations
 
 DIR = os.path.dirname(os.path.abspath(__file__))
@@ -15,17 +15,16 @@ BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
 # These revisions are all tag experimental branches off the same revision.
 # we only need different tags so lab creates separate build directories in the build cache.
 # We then manually recompile the code in the build cache with the correct settings.
-REVISIONS = ["issue980v2"]
+REVISIONS = ["7a9dc9fd2f6ab01112f8f0b8c8bca26d371b1a1c"]
 CONFIGS = [
     # IssueConfig("blind", ["--search", "astar(blind())"]),
-    IssueConfig("shortest-blind", ["--search", "shortest_astar(blind())"]),
+    IssueConfig("blind", ["--search", "astar(blind())"]),
     IssueConfig("lmcut", ["--search", "astar(lmcut())"]),
-    IssueConfig("shortest-lmcut", ["--search", "shortest_astar(lmcut())"]),
 
-    IssueConfig("shortest-ms", ["--search", "shortest_astar(merge_and_shrink(transform=no_transform(), cache_estimates=true, merge_strategy=merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),shrink_strategy=shrink_strategy=shrink_bisimulation(greedy=false), prune_unreachable_states=true, prune_irrelevant_states=true, max_states=-1, max_states_before_merge=-1, threshold_before_merge=-1, verbosity=normal, main_loop_max_time=infinity))"]),
-    IssueConfig("shortest-cegar", ["--search", "shortest_astar(cegar())"]),
-    IssueConfig("shortest-hmax", ["--search", "shortest_astar(hmax())"]),
-    IssueConfig("shortest-ipdb", ["--search", "shortest_astar(ipdb(pdb_max_size=2000000, collection_max_size=20000000, num_samples=1000, min_improvement=10, max_time=infinity, random_seed=-1, max_time_dominance_pruning=infinity, transform=no_transform(), cache_estimates=true))"]),
+    IssueConfig("ms", ["--search", "astar(merge_and_shrink(transform=no_transform(), cache_estimates=true, merge_strategy=merge_strategy=merge_sccs(order_of_sccs=topological,merge_selector=score_based_filtering(scoring_functions=[goal_relevance,dfp,total_order])),shrink_strategy=shrink_strategy=shrink_bisimulation(greedy=false), prune_unreachable_states=true, prune_irrelevant_states=true, max_states=-1, max_states_before_merge=-1, threshold_before_merge=-1, verbosity=normal, main_loop_max_time=infinity))"]),
+    IssueConfig("cegar", ["--search", "astar(cegar())"]),
+    IssueConfig("hmax", ["--search", "astar(hmax())"]),
+    IssueConfig("ipdb", ["--search", "astar(ipdb(pdb_max_size=2000000, collection_max_size=20000000, num_samples=1000, min_improvement=10, max_time=infinity, random_seed=-1, max_time_dominance_pruning=infinity, transform=no_transform(), cache_estimates=true))"]),
 ]
 
 SUITE = ["agricola-opt18-strips", "barman-opt11-strips", "data-network-opt18-strips", "elevators-opt08-strips", "elevators-opt11-strips", "floortile-opt11-strips", "floortile-opt14-strips", "ged-opt14-strips", "openstacks-opt08-strips", "openstacks-opt11-strips", "openstacks-opt14-strips", "organic-synthesis-split-opt18-strips", "parcprinter-08-strips", "parcprinter-opt11-strips", "pegsol-08-strips", "pegsol-opt11-strips", "petri-net-alignment-opt18-strips", "scanalyzer-08-strips", "scanalyzer-opt11-strips", "sokoban-opt08-strips", "sokoban-opt11-strips", "spider-opt18-strips", "tetris-opt14-strips", "transport-opt08-strips", "transport-opt11-strips", "transport-opt14-strips", "woodworking-opt08-strips", "woodworking-opt11-strips"]
@@ -48,16 +47,11 @@ exp.add_parser(exp.PLANNER_PARSER)
 exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
 exp.add_fetcher(name='fetch')
-
+exp.add_fetcher('data/issue980-v1-eval')
 attributes = (
             IssueExperiment.DEFAULT_TABLE_ATTRIBUTES + ["plan_length"])
-#exp.add_absolute_report_step(attributes=attributes)
+exp.add_absolute_report_step(attributes=attributes)
 #exp.add_comparison_table_step(attributes=attributes)
-
-nicks = ["shortest-blind", "shortest-lmcut", "shortest-ms", "shortest-cegar", "shortest-hmax", "shortest-ipdb"]
-
-algs = ["%s-%s" % (r, nick) for r in REVISIONS for nick in nicks ]
-exp.add_absolute_report_step(attributes=attributes,filter_algorithm=algs)
 
 
 #exp.add_comparison_table_step()
@@ -70,4 +64,18 @@ for r1, r2 in combinations(REVISIONS, 2):
             get_category=lambda run1, run2: run1["domain"]),
             outfile="issue925-v1-total-time-%s-%s-%s.png" % (r1, r2, nick))
 """
+
+alg_names = ["blind", "cegar", "hmax", "ipdb", "lmcut", "ms"]
+rev1 = "7a9dc9fd2f6ab01112f8f0b8c8bca26d371b1a1"
+rev2 = "issue980v2-shortest"
+
+attrs = ["total_time", "reopened", "memory", "expansions", "expansions_until_last_jump"]
+for attr in attrs:
+    for nick in alg_names:
+        exp.add_report(RelativeScatterPlotReport(
+                    attributes=attr,
+                                filter_algorithm=["%s-%s" % (r, nick) for r in [rev1, rev2]],
+                                            get_category=lambda run1, run2: run1["domain"]),
+                                                        outfile="issue980-base-%s-%s-%s-%s.png" % (attr, r1, r2, nick))
+
 exp.run_steps()
--- experiments/issue980/v2.py
+++ experiments/issue980/v2.py
@@ -49,6 +49,9 @@ exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
 exp.add_fetcher(name='fetch')
 
+exp.add_fetcher('data/issue980-v1-eval')
+exp.add_fetcher('data/issue980-consistent-eval')
+exp.add_fetcher('data/issue980-por-eval')
 attributes = (
             IssueExperiment.DEFAULT_TABLE_ATTRIBUTES + ["plan_length"])
 #exp.add_absolute_report_step(attributes=attributes)
@@ -57,6 +60,11 @@ attributes = (
 nicks = ["shortest-blind", "shortest-lmcut", "shortest-ms", "shortest-cegar", "shortest-hmax", "shortest-ipdb"]
 
 algs = ["%s-%s" % (r, nick) for r in REVISIONS for nick in nicks ]
+
+algs.extend(["0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-cegar", "0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-hmax","0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-ipdb", "0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-ms"])
+
+algs.append('a0eb9a43ba45f8f9817d99ee7b8ea9676937a0a9-shortest-lmcut-oss-por')
+
 exp.add_absolute_report_step(attributes=attributes,filter_algorithm=algs)
 
 
