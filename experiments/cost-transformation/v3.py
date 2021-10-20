a256021bd19de63e0e1dda3a3d1e54b542f143d7
Michael Katz <michael.katz1@ibm.com>
2021-10-20 14:29:46 -0400

    added to the exp file

--- experiments/cost-transformation/v3.py
+++ experiments/cost-transformation/v3.py
@@ -48,24 +48,23 @@ exp.add_step('build', exp.build)
 exp.add_step('start', exp.start_runs)
 exp.add_fetcher(name='fetch')
 
-# exp.add_fetcher('data/issue980-v1-eval')
-# exp.add_fetcher('data/issue980-consistent-eval')
-# exp.add_fetcher('data/issue980-por-eval')
+exp.add_fetcher('/data/software/shortest-structural-symmetries-pruning/experiments/issue980/data/issue980-v5-DEval-all-eval')
+
 attributes = (
             IssueExperiment.DEFAULT_TABLE_ATTRIBUTES + ["plan_length"])
-#exp.add_absolute_report_step(attributes=attributes)
 #exp.add_comparison_table_step(attributes=attributes)
-exp.add_absolute_report_step(attributes=attributes)
+# exp.add_absolute_report_step(attributes=attributes)
 
-nicks = ["shortest-blind", "shortest-lmcut", "shortest-ms", "shortest-cegar", "shortest-hmax", "shortest-ipdb"]
 
-algs = ["%s-%s" % (r, nick) for r in REVISIONS for nick in nicks ]
+# nicks = ["shortest-blind", "shortest-lmcut", "shortest-ms", "shortest-cegar", "shortest-hmax", "shortest-ipdb"]
+nicks = ["blind", "lmcut", "ms", "cegar", "hmax", "ipdb"]
 
-algs.extend(["0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-cegar", "0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-hmax","0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-ipdb", "0de9a050a11d7142d5c2a56fa94cd9c7b3eb94ab-shortest-ms"])
+algs = ["%s-ct-%s" % (r, nick) for r in REVISIONS for nick in nicks ]
+algs.extend(["c292531fa6cdbeae95a0bf576fd50a65967ed5cc-shortest-%s" % nick for nick in nicks ])
 
-algs.append('a0eb9a43ba45f8f9817d99ee7b8ea9676937a0a9-shortest-lmcut-oss-por')
 
-# exp.add_absolute_report_step(attributes=attributes,filter_algorithm=algs)
+
+exp.add_absolute_report_step(attributes=attributes,filter_algorithm=algs)
 
 
 #exp.add_comparison_table_step()
